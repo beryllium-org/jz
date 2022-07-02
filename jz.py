0612cb953c5dd65d73647135c042ea18655cb848
Bill Sideris <bill88t@feline.gr>
2022-07-02 18:25:33 +0300

    1.1 fixed cross directory source files, changed exits to returns

--- jz.py
+++ jz.py
@@ -1,9 +1,9 @@
 import zlib
 from gc import collect
-from sys import argv, exit
+from sys import argv
 from os import getcwd, chdir
 
-VERSION = "1.0"
+VERSION = "1.1"
 
 def compress(*argv):
     """
@@ -22,19 +22,21 @@ def compress(*argv):
     # the control string format is:
     #   file1 file1len file2 file2len |eocf
     for i in range(0, objc):
-        ctlstr += f"{argv[i]} " # do not remove the whitespace
+        fnamee = argv[i]
+        if fnamee.find('/') != -1:
+            # file not in cwd, we have to cut the name for the control string
+            fnamee = fnamee[fnamee.rfind("/",1)+1:] # remove everything up until the last /, including the slash
+        ctlstr += f"{fnamee} " # do not remove the whitespace
         try:
             with open(argv[i], "rb") as togetlelen:
                 inpt = togetlelen.read()
-                togetlelen.close()
-                del togetlelen
                 ctlstr += f"{str(len(inpt))} " # do not remove the whitespace
-                print(f"Loading: {argv[i]} ({str(len(inpt))} bytes)")
+                print(f"Loading: {fnamee} ({str(len(inpt))} bytes)")
                 datastr += inpt #copying bytes, not str
                 del inpt
         except OSError:
-            print(f"Error: Could not open file {argv[i]}")
-            exit(1)
+            print(f"Error: Could not open file {argv[i]}") # intentionally letting it show the full path
+            return(1)
         del i
         collect()
         collect()
@@ -55,12 +57,10 @@ def compress(*argv):
         jzfile.write(out)
         del out
         jzfile.flush()
-        jzfile.close()
-        del jzfile
     del targetfile
     
     #done
-    exit(0)
+    return(0)
 
 def decompress(filee, directory="."):
     print(f"Decompressing {filee} into {directory if directory != '.' else 'the current directory'}")
@@ -68,8 +68,6 @@ def decompress(filee, directory="."):
     #dump file inputted
     with open(filee,"rb") as inpf:
         dataa = inpf.read()
-        inpf.close()
-        del inpf
     
     #switch to target dir
     olddir = getcwd()
@@ -96,11 +94,9 @@ def decompress(filee, directory="."):
             with open(fname,"wb") as fout:
                 fout.write(unz[offset:offset+lco])
                 fout.flush()
-                fout.close()
-                del fout
         except OSError:
             print("Error: Could not write file")
-            exit(1)
+            return(1)
         offset += lco
         del lco, fname
         collect()
@@ -112,7 +108,7 @@ def decompress(filee, directory="."):
     #switch back to start dir
     chdir(olddir)
     del olddir
-    exit(0)
+    return(0)
 
 def help():
     print(f"""
@@ -122,4 +118,4 @@ def help():
         Usage: jz.compress(\"file1\", \"file2\", \"jz_archive_name\")
                jz.decompress(\"jz_archive_name\")
     """)
-    exit(0)
+    return(0)
